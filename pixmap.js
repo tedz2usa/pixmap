5dc706078db0cdabe06d25b48500c003853dfeec
Ted Zhu <tedz2usa@gmail.com>
2016-02-29 02:26:41 -0500

    Add basic methods for creating dom and add a window onload function.

--- pixmap.js
+++ pixmap.js
@@ -1,3 +1,32 @@
 var log = console.log.bind(console);
 
 log('Hello');
+
+
+window.addEventListener('load', init);
+
+function init() {
+	log('Window loaded!');
+}
+
+
+
+
+
+
+
+
+function createDiv(classes) {
+	return createDom('div', classes);
+}
+
+
+function createDom(tagname, classes) {
+	var elem = document.createElement(tagname);
+	elem.className = classes;
+	return elem;
+}
+
+
+
+
